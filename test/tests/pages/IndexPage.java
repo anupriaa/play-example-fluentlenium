d7a157a6f3996dba38151cfbdb7c09c723709954
Philip Johnson <johnson@hawaii.edu>
2015-03-04 15:19:30 -1000

    Minor typo fixes.

--- test/tests/pages/IndexPage.java
+++ test/tests/pages/IndexPage.java
@@ -2,14 +2,12 @@ package tests.pages;
 
 import org.fluentlenium.core.FluentPage;
 import org.openqa.selenium.WebDriver;
-// Although Eclipse marks the following two methods as deprecated, 
-// the no-arg versions of the methods used here are not deprecated.  (as of May, 2013).
 import static org.fluentlenium.core.filter.FilterConstructor.withText; 
 import static org.fluentlenium.core.filter.FilterConstructor.withId;  
 import static org.fest.assertions.Assertions.assertThat;
 
 /**
- * Illustration of the Page Object Pattern in Fluentlenium.  
+ * Provides testing support for the Index page.
  * @author Philip Johnson
  */
 public class IndexPage extends FluentPage {
